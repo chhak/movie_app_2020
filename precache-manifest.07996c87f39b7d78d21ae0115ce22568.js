f2869309289efd36373d116201c9a896bc98a295
aneklfae <anfekl@nael.com>
2020-10-16 12:58:04 +0900

    Updates

---
+++ precache-manifest.07996c87f39b7d78d21ae0115ce22568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789cde7ac099e828e511d38884b6396b",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "c13ff78d343adfffbe3f",
+    "url": "/movie_app_2020/static/css/main.47b959d5.chunk.css"
+  },
+  {
+    "revision": "5e897be818a87bd43ec8",
+    "url": "/movie_app_2020/static/js/2.fad5d4a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.fad5d4a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13ff78d343adfffbe3f",
+    "url": "/movie_app_2020/static/js/main.0f54c94f.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
